359b29592232bd4073e676b7324b580bb588a76e
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-03-18 14:59:45 +1030

    feat: Define markdown generator functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,11 @@
+// TODO: Return README screenshot section
+// If there is no screenshot URL, return an empty string
+function renderScreenshot(screenshotUrl, projectTitle) {}
+
+// TODO: Return README live demo link
+// If there is no demo link URL, return an empty string
+function renderLiveDemoLink(liveDemoUrl) {}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -6,15 +14,23 @@ function renderLicenseBadge(license) {}
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
+// TODO: Return license TOC entry
+// If there is no license, return an empty string
+function renderLicenseToc(license) {}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// TODO: Return questions TOC entry
+// If neither GitHub username nor Email address have been provided, return an empty string
+function renderQuestionsToc(username, email) {}
 
-`;
-}
+// TODO: Return questions section
+// If neither GitHub username nor Email address have been provided, return an empty string
+function renderQuestionsSection(username, email) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {}
 
 module.exports = generateMarkdown;
